09fea084506ad1b1c51aa373601f47bbb846042d
Tirth Patel <tirthp292003@gmail.com>
2025-03-16 00:24:16 +0530

    Final Commit

--- server/server.js
+++ server/server.js
@@ -16,6 +16,31 @@ const app = express();
 await connectDB();
 await connectCloudinay();
 
+const allowedOrigins = [
+  "https://edemy-frontend-gsf5bhjh8-tirthppatel222-gmailcoms-projects.vercel.app",
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "http://localhost:5173",
+  "https://edemyy.vercel.app",
+  "https://edemy-frontend-xi.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `🚫 CORS policy does not allow access from origin ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 // middleware
 app.use(cors());
 app.use(clerkMiddleware());
