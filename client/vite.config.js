6269b1387474a7e25d38cd4d5825fc8b9c82bf6c
Tirth Patel <tirthp292003@gmail.com>
2025-03-16 11:29:40 +0530

    Project Completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["youtube-player"], // <-- important for dev
+  },
   build: {
     rollupOptions: {
-      external: ["youtube-player"],
+      external: [],
     },
   },
   assetsInclude: ["**/*.JPG"],
